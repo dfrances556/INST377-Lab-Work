729b72817041df619027da004cd0c97c87fbb60c
dfrances556 <dfrances@umd.edu>
2022-09-23 00:56:30 -0400

    fixes off by 1 error

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -24,6 +24,7 @@ function updateSlidePosition() {
     slide.classList.add('hidden');
   });
 
+  console.log(slidePosition);
   slides[slidePosition].classList.add('visible');
 }
 
@@ -34,7 +35,7 @@ function moveToNextSlide() {
     and if so, sets your slidePosition to the first index of an array
     if not, set the slidePosition to the current position plus one
   */
-  if (slidePosition === totalSlides) {
+  if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
     slidePosition += 1;
